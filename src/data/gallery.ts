5d4ba8bc9eb14c7461b2433732119bc488bf1b24
Ulrichjack <jackeboyeur244@gmail.com>
2026-08-20 15:45:35 +0100

    feat: update social action gallery photos

--- src/data/gallery.ts
+++ src/data/gallery.ts
@@ -10,11 +10,12 @@ export const galleryAssets = [
 
 /** Photos des actions sociales de COST, optimisées au format WebP. */
 export const socialActionGalleryAssets = [
-  { src: '/images/gallery_action_social/01-construction-point-eau.webp', width: 780, height: 939 },
-  { src: '/images/gallery_action_social/02-bassin-point-eau.webp', width: 780, height: 943 },
-  { src: '/images/gallery_action_social/03-travaux-forage.webp', width: 780, height: 949 },
-  { src: '/images/gallery_action_social/04-amenagement-point-eau.webp', width: 780, height: 959 },
-  { src: '/images/gallery_action_social/05-forage-communautaire.webp', width: 780, height: 941 },
-  { src: '/images/gallery_action_social/06-suivi-travaux.webp', width: 780, height: 930 },
-  { src: '/images/gallery_action_social/07-equipe-chantier.webp', width: 720, height: 1280 },
+  { src: '/images/gallery_action_social/01-pompe-point-eau.webp', width: 977, height: 1610 },
+  { src: '/images/gallery_action_social/02-construction-point-eau.webp', width: 1149, height: 1369 },
+  { src: '/images/gallery_action_social/03-bassin-point-eau.webp', width: 1183, height: 1330 },
+  { src: '/images/gallery_action_social/04-amenagement-point-eau.webp', width: 1131, height: 1391 },
+  { src: '/images/gallery_action_social/05-travaux-forage.webp', width: 1136, height: 1385 },
+  { src: '/images/gallery_action_social/06-forage-communautaire.webp', width: 1142, height: 1377 },
+  { src: '/images/gallery_action_social/07-suivi-travaux.webp', width: 1148, height: 1371 },
+  { src: '/images/gallery_action_social/08-equipe-chantier.webp', width: 941, height: 1672 },
 ] as const;
